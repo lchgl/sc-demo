edcc2acc28a860a3b5b8771f68151bcc19f432c5
lchgl <403552501@qq.com>
2018-07-04 18:23:30 +0800

    null

--- Component/Main/XMGMain.js
+++ Component/Main/XMGMain.js
@@ -5,76 +5,102 @@
  */
 
 import React, { Component } from 'react';
+// import {
+//   Platform,
+//   StyleSheet,
+//   Text,
+//   View,
+//   Image
+// } from 'react-native';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Image
-} from 'react-native';
-import TabNavigator from 'react-native-tab-navigator';
+    createStackNavigator
+} from 'react-navigation';
+import {
+    createBottomTabNavigator,
+    BottomTabBar
+} from 'react-navigation-tabs';
+
 import Home from '../Home/XMGHome';
 import Mine from '../Mine/XMGMine';
 import More from '../More/XMGMore';
 import Shop from '../Shop/XMGShop';
 
-type Props = {};
-export default class Main extends Component<Props> {
-  //初始化页面变量
-    constructor(props){
-        super(props);
-        // const page = this.props.navigation.state.page?this.props.navigation.state.page:'首页';
-        this.state = {
-            selectTab:'home'
-        }
-    }
-  render() {
-    return (
-      <TabNavigator>
-        <TabNavigator.Item
-            title='首页'
-            renderIcon={() => <Image style={styles.iconStyle} source={require('../../src/images/tab/tab-main-2.png')} />}
-            rederSelectedIcon={() => <Image source={require('../../src/images/tab/tab-main-1.png')} style={styles.iconStyle}/>}
-            onPress={()=>{this.setState({selectTab:'home'})}}
-            selected={this.state.selectTab === 'home'}
-        >
-          <Home/>
-        </TabNavigator.Item>
-          <TabNavigator.Item
-              title='商家'
-              renderIcon={() => <Image style={styles.iconStyle} source={require('../../src/images/tab/tab-shop-2.png')} />}
-              rederSelectedIcon={() => <Image source={require('../../src/images/tab/tab-shop-1.png')} style={styles.iconStyle}/>}
-              onPress={()=>{this.setState({selectTab:'shop'})}}
-              selected={this.state.selectTab === 'shop'}
-          >
-            <Shop/>
-          </TabNavigator.Item>
-          <TabNavigator.Item
-              title='我的'
-              renderIcon={() => <Image style={styles.iconStyle} source={require('../../src/images/tab/tab-home-2.png')} />}
-              rederSelectedIcon={() => <Image source={require('../../src/images/tab/tab-home-1.png')} style={styles.iconStyle}/>}
-              onPress={()=>{this.setState({selectTab:'mine'})}}
-              selected={this.state.selectTab === 'mine'}
-          >
-            <Mine/>
-          </TabNavigator.Item>
-          <TabNavigator.Item
-              title='更多'
-              renderIcon={() => <Image style={styles.iconStyle} source={require('../../src/images/tab/tab-more-2.png')} />}
-              rederSelectedIcon={() => <Image source={require('../../src/images/tab/tab-more-1.png')} style={styles.iconStyle}/>}
-              onPress={()=>{this.setState({selectTab:'more'})}}
-              selected={this.state.selectTab === 'more'}
-          >
-            <More/>
-          </TabNavigator.Item>
-      </TabNavigator>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  iconStyle:{
-    width:Platform.OS === 'ios' ? 30 : 25,
-    height:Platform.OS === 'ios' ? 30 : 25,
-  }
-});
+const TabBarComponent = (props) => (<BottomTabBar {...props} />);
+
+const TabScreens = createBottomTabNavigator(
+    {
+      '主页':{screen:Home},
+      '商家':{screen:Shop},
+      '我的':{screen:Mine},
+      '更多':{screen:More}
+    },
+    {
+        tabBarComponent: props =>
+            <TabBarComponent
+                {...props}
+                style={{ borderTopColor: '#605F60' }}
+            />,
+        tabBarPosition: 'bottom',
+        animationEnabled: false,
+        tabBarOptions: {
+            activeTintColor: '#D6D6D6',
+            inactiveTintColor: '#605F60',
+            inactiveBackgroundColor: '#fff',
+            activeBackgroundColor: '#fff',
+            labelStyle: {
+                fontSize: 16,
+                paddingBottom: 3,
+            },
+            style: {
+                backgroundColor: '#fff',
+            },
+        },
+        showIcon: true,
+        swipeEnabled: true,
+    },
+);
+const myNavigator=createStackNavigator(
+    {
+        TabNav:{
+            screen:TabScreens,
+            navigationOptions: ({ navigation }) => ({
+                // title: 'Home',
+                header:null,
+            }),
+        },
+        // Home:Home
+    },
+    {
+        initialRouteName:'TabNav',
+    }
+);
+export default myNavigator
+
+// export default TabScreens
+
+// export default class Main extends Component<Props> {
+//   //初始化页面变量
+//     constructor(props){
+//         super(props);
+//         // const page = this.props.navigation.state.page?this.props.navigation.state.page:'首页';
+//         this.state = {
+//             selectTab:'home'
+//         }
+//     }
+//   render() {
+//     return (
+//       <View>
+//           <Text>main入口</Text>
+//           <TabScreens></TabScreens>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   iconStyle:{
+//     width:Platform.OS === 'ios' ? 30 : 25,
+//     height:Platform.OS === 'ios' ? 30 : 25,
+//   }
+// });
